1d2c7a17085a901b70ef9f50413f2e9a65b2b434
May Kaung Kin <65530728+MaySky99@users.noreply.github.com>
2020-07-03 17:06:47 +0630

    addedfooter for application.php

---
+++ footer.php
@@ -0,0 +1,6 @@
+<footer>
+        <div class="container-fluid bg-info mt-5 p-2 text-center">
+            <p class="h5">Copyright&copy; All Rights Reserved By Oxford University</p>
+        </div>
+        
+</footer>
