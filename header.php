dfdf0fb6a5fdb4c775b371c02e7ac6ffdce50b53
May Kaung Kin <65530728+MaySky99@users.noreply.github.com>
2020-07-03 17:06:10 +0630

    addedheader for application.php

---
+++ header.php
@@ -0,0 +1,47 @@
+<header>
+        <div class="container-fluid">
+            <nav class="navbar navbar-expand-sm bg-info p-3">
+                <a class="navbar-brand ml-3 mr-5" href="#"><img src="img/oxford_logo.png" width="250px" height="70px"></a>
+                
+                <ul class="navbar-nav">
+                  <li class="nav-item">
+                    <a class="nav-link ml-3" href="#">Home</a>
+                  </li>
+                  <li class="nav-item">
+                    <a class="nav-link ml-3" href="#">Online Application</a>
+                  </li>
+                  <li class="nav-item">
+                    <a class="nav-link ml-3" href="#">Login</a>
+                  </li>
+                </ul>
+              </nav>             
+        </div>
+
+        <div class="container">
+            <img src="img/main.jpg" class="img-fluid">
+        </div>
+
+        
+        <div class="container mt-3">
+            <p class="h3">Admission Progress</p>
+            <div class="row mt-3">
+                <div class="col-3">
+                    <p>1.Fill in Application</p>
+                </div>
+                <div class="col-2">
+                    <p>2.Verify Data</p>
+                </div>
+                <div class="col-4">
+                    <p>3.Print Application Form</p>
+                </div>
+                <div class="col-2">
+                    <p>4.Login</p>
+                </div>
+            </div>
+            <div class="progress">
+                <div class="progress-bar progress-bar-striped" style="width:25%"></div>
+            </div><br><br>
+            <div class="bottom"></div>
+        </div>
+
+</header>
